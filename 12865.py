6313c8d91e4800ca7bcab093b18c5dd2328af0e2
eugene <dign106@gmail.com>
2022-08-06 02:27:02 +0900

    light ver

--- 12865.py
+++ 12865.py
@@ -6,31 +6,30 @@ for i in range(0,N):
   a,b=list(map(int,input().split()))
   W.append(a)
   V.append(b)
+use=[]
 dp=[]
-use_s_num=0
-for i in range(0,K+1):
-  dp.append(0)
-for i in range(0,N+1):
-  use_s_num=0
+for i in range(0,2):
+  a=[]
   for j in range(0,K+1):
-    if W[i]<=j:
-      if use_s_num==0:
-        dp[j] = max((dp[j-W[i]]+V[i]),dp[j],dp[j-1])
-        if dp[j]==dp[j-W[i]]+V[i] | j==V[i]:
-          use_s_num=-1
+    a.append(0)
+  dp.append(a)
+
+for i in range(1,N+1):
+  temp_i=i%2
+  for j in range(1,K+1):
+    if temp_i==0:
+      if W[i]<=j: 
+        dp[temp_i][j] = max((dp[temp_i+1][j-W[i]]+V[i]),dp[temp_i+1][j])
       else:
-        dp[j]=max(dp[j],dp[j-1])
-
-  
-  print(dp)
-print(max(dp))
-
-
-
-        
-
-
-    
-
-
-
+        dp[temp_i][j]=dp[temp_i+1][j]    
+    else:
+      if W[i]<=j: 
+        dp[temp_i][j] = max((dp[temp_i-1][j-W[i]]+V[i]),dp[temp_i-1][j])
+      else:
+        dp[temp_i][j]=dp[temp_i-1][j]      
+#  print(dp)
+if N%2==0:
+  print(dp[0][K])
+else:
+  print(dp[1][K])
+#print(dp[N][K])
